1b334103d5c314446baa7bca32281628cd492158
AnHgPham <an.113tin.cva@gmail.com>
2026-02-04 21:58:29 +0700

    update

--- frontend/src/layouts/AdminLayout.js
+++ frontend/src/layouts/AdminLayout.js
@@ -1,14 +1,1 @@
-import AdminNavbar from "@/components/admin/navbar";
 
-const AdminLayout = ({ children }) => {
-  return (
-    <div className="flex-row relative">
-      <div className="fixed top-0">
-        <AdminNavbar />
-      </div>
-      {children}
-    </div>
-  );
-};
-
-export default AdminLayout;
