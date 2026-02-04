e08ded3ef42e1dce459a94b011b9d9745b75fc85
AnHgPham <an.113tin.cva@gmail.com>
2026-02-04 21:56:12 +0700

    update layout

--- frontend/src/layouts/AdminLayout.js
+++ frontend/src/layouts/AdminLayout.js
@@ -2,7 +2,7 @@ import AdminNavbar from "@/components/admin/navbar";
 
 const AdminLayout = ({ children }) => {
   return (
-    <div className="flex flex-row relative">
+    <div className="flex-row relative">
       <div className="fixed top-0">
         <AdminNavbar />
       </div>
